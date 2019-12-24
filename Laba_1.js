3f9875119fee96cf08541b04840fa9ecd8a6d22d
GreatGleb <44549953+GreatGleb@users.noreply.github.com>
2019-12-24 12:19:20 +0200

    Update Laba_1.js

--- Laba_1.js
+++ Laba_1.js
@@ -85,11 +85,13 @@ function AdditionDK (n1, n2) {
 				str = str+"Разряд "+(len1-i)+" Складываем "+ost[len1-i+1]+", "+arr1[len1-i]+" и "+arr2[len1-i]+"\nПолучаем: "+ost[len1-i]+arr3[len1-i]+" Перенос: "+ost[len1-i]+"<br>";
 			}
 		}
+	
+		arr3.splice(1,0,".");
+		var result = arr3.join("");
+		
 		if(ost[0]==ost[1]) {
 			//console.log("\nПереполнения не было.");
 			str = str+"Переполнения не было."+"<br>";
-			arr3.splice(1,0,".");
-			var result = arr3.join("");
 			str = str+"Результат сложения: "+result+"</div>";
 		} else {
 			//console.log("\nБыло переполнение.");
@@ -326,7 +328,167 @@ function AdditionOK (n1, n2) {
 		  }; 
 }
 
+function IsBigger(n1, n2) {
+	var arr1 = n1.toString().split('');
+	if(arr1[1]==".") {
+	arr1.splice(1,1); }
+	var arr2 = n2.toString().split('');
+	if(arr2[1]==".") {
+	arr2.splice(1,1); }
+	
+	arr1.splice(0,1);
+	arr2.splice(0,1); 
+	
+	let i = 0;
+	while (i < arr1.length) { // выводит 0, затем 1, затем 2
+	  if(arr1[i]>arr2[i]) {
+		  return 0;
+	  } else if (arr1[i]<arr2[i]) {
+		  return 1;
+	  }
+	  i++;
+	}
+}
 
+function Difference (n1, n2) {
+	var arr1 = n1.toString().split('');
+	if(arr1[1]==".") {
+	arr1.splice(1,1); }
+	var arr2 = n2.toString().split('');
+	if(arr2[1]==".") {
+	arr2.splice(1,1); }	
+	arr1.splice(0,1);
+	arr2.splice(0,1); 
+	let len1 = arr1.length;
+	let ost = [];
+	let arr3 = [];
+	let sum = [];
+	
+	for(var i = 1; i < len1+1; i++) {
+			if(((parseInt(arr1[len1-i],10)==0)&&(parseInt(arr2[len1-i],10)==0))
+				||((parseInt(arr1[len1-i],10)==1)&&(parseInt(arr2[len1-i],10)==1))) {
+					arr3[len1-i] = 0;
+			} else if((parseInt(arr1[len1-i],10)==0)&&(parseInt(arr2[len1-i],10)==1)) {
+				arr3[len1-i] = 1;
+				arr1[len1-i-1]= parseInt(arr1[len1-i-1],10)-1;
+			} else if((parseInt(arr1[len1-i],10)==1)&&(parseInt(arr2[len1-i],10)==0)) {
+				arr3[len1-i] = 1;
+			} else if((parseInt(arr1[len1-i],10)<0)&&(parseInt(arr2[len1-i],10)==0)) {
+				arr3[len1-i] = 0;
+				arr1[len1-i-1]= parseInt(arr1[len1-i-1],10)-1;
+			} else if((parseInt(arr1[len1-i],10)<0)&&(parseInt(arr2[len1-i],10)==1)) {
+				arr3[len1-i] = 0;
+				arr1[len1-i-1]= parseInt(arr1[len1-i-1],10)-1;
+			}
+		}
+	return arr3.join('');
+}
+
+function AdditionPK (n1, n2) {
+	var arr1 = n1.toString().split('');
+	if(arr1[1]==".") {
+	arr1.splice(1,1); }
+	var arr2 = n2.toString().split('');
+	if(arr2[1]==".") {
+	arr2.splice(1,1); }
+	
+	var str = "";
+	var arr3 = [];
+	var ost = [];
+	var ost2 = [];
+	var arr4 = [];
+	
+	var len1 = arr1.length;
+	var len2 = arr2.length;
+			
+	var ZnA = arr1[0];
+	var ZnB = arr2[0];
+	var result;
+	
+	if(len1 == len2) {
+	
+		if(arr1[0]==arr2[0]) {
+			arr1.splice(0,1);
+			arr2.splice(0,1); 
+			
+			len1 = len1 - 1;
+			len2 = len2 - 1;
+	
+			for(var i = 1; i < len1+1; i++) {
+				if((ost[len1-i+1]==undefined)||(ost[len1-i+1]==0)) {
+					var sum = parseInt(arr1[len1-i],10)+parseInt(arr2[len2-i],10);
+					if(sum > 2.9) {
+						arr3[len1-i] = 1;
+						ost[len1-i] = 1;
+					}
+					else if((sum > 1.1)&&(sum < 2.9)) {
+						arr3[len1-i] = 0;
+						ost[len1-i] = 1;
+					} else {
+						arr3[len1-i] = sum;
+						ost[len1-i] = 0;
+					}
+				} else {
+					var sum = parseInt(arr1[len1-i],10)+parseInt(arr2[len2-i],10)+ost[len1-i+1];
+					if(sum > 2.9) {
+						arr3[len1-i] = 1;
+						ost[len1-i] = 1;
+					}
+					else if((sum > 1.1)&&(sum < 2.9)) {
+						arr3[len1-i] = 0;
+						ost[len1-i] = 1;
+					} else {
+						arr3[len1-i] = sum;
+						ost[len1-i] = 0;
+					}
+				}
+			}
+		
+			arr3.splice(0,0, ZnA);
+			arr3.splice(1,0,".");
+			result = arr3.join("");
+			arr3.splice(1,1);
+
+			if(ost[0]==0) {
+				//console.log("\nПереполнения не было.");
+				str = str+"Переполнения не было."+"<br>";
+				str = str+"Результат сложения: "+result+"</div>"+"</p>";
+			} else {
+				//console.log("\nБыло переполнение.");
+				str = str+'<p style="color: red ">Было переполнение.</p>';
+			}
+		} else {
+			if(IsBigger(n1, n2)==0) {
+				var S = Difference(n1, n2).split('');
+				S.splice(0,0, ZnA);
+				S.splice(1,0,".");
+			} else {
+				var S = Difference(n2, n1).split('');
+				S.splice(0,0, ZnB);
+				S.splice(1,0,".");
+			}
+			result = S.join('');
+			str = str+"Переполнения не было."+"<br>";
+			str = str+"Результат сложения: "+S.join('')+"</div>"+"</p>";
+		}
+				
+		return {
+		    addition: function() {
+		      return result;
+		    },
+		    result: function() {
+		      return str;
+		    }
+		  };   
+
+	}
+	str = str+'<p style="color: red ">Числа с разным колличеством разрядов!</p>';
+	return  {
+		    result: function() {
+		      return str;
+		    }
+		  }; 
+}
 
 function ProductionOK (n1, n2) {
 	console.log("Ehhhuuuuu!"); 
@@ -342,7 +504,6 @@ function ProductionOK (n1, n2) {
     var arr4 = []; 
     var len1 = arr1.length; 
     var len2 = arr2.length; 
-    var str = "<div> ОК<br>"+n1+"<br>"+n2+"<br>"; 
 
     console.log(arr1); 
 
@@ -490,6 +651,178 @@ function ProductionOK (n1, n2) {
 	return RESULT;
 }
 
+function OtricPK(n1) {
+	let arr = n1.toString().split('');
+	
+	if (arr[0] == "1") {
+		arr[0] = 0;
+	} else if (arr[0]=="0") {
+		arr[0] = 1;
+	}
+	
+	return arr.join('');
+}
+
+function OtricDK(n) {
+	let arr = n.toString().split('');
+	
+	for(let i = 2; i < arr.length; i++) {
+		if (arr[i] == "1") {
+			arr[i] = 0;
+		} else if (arr[i]=="0") {
+			arr[i] = 1;
+		}
+	}
+	
+	return arr.join('');
+}
+
+function Sdvig(n) {
+	n = n.split('');
+	n.splice(0,2);
+	n.push('0');
+	n.splice(1,0,".");
+	return n.join('');
+}
+
+function SdvigPr(n, t) {
+	n = n.split('');
+	for(let i = 0; i < t; i++) {
+		n.splice(2,0,'0');
+	}
+	return n.join('');
+}
+
+function Drob10(n) {
+	n = n.split('');
+	let zn = n[0];
+	let sum = 0;
+	for(let i = 2; i < n.length; i++) {
+		if(n[i]=='1') {
+			i--;
+			let b = i*(-1);
+			sum = sum + Math.pow(2,b);
+			i++;
+		}
+	}
+	if (zn =='1') {
+		sum = sum*(-1);
+	}
+	sum = sum.toString();
+	return sum;
+}
+
+function DivisionPK (n1, n2) {
+	console.log("Ehhhuuuuu!");
+	
+    var arr1 = n1.toString().split('');
+    arr1.splice(1,1);
+	
+	let dlme;
+	var dltl = OtricPK(n2);
+	dltl = OtricDK(dltl);
+	let dt10 = Drob10(n2);
+	let res = [];
+	
+	if(arr1[0] == "1") {
+		dlme = OtricDK(n1);
+	} else {
+		dlme = n1;
+	}
+	
+	var edin = [];
+	for(let i = 0; i < arr1.length; i++) {
+		if(i<(arr1.length-1)) {
+			edin[i] = 0;
+		} else {
+			edin[i] = 1;
+		}
+	}
+	edin.splice(1,0,".");
+	edin = edin.join("");
+
+	dltl = AdditionDK(dltl, edin).addition();
+	
+	let hg = AdditionDK(dlme, dltl).addition();
+    var str = "<div> Bloouuu.. Ahhh!<br>"+"ДМ = "+Drob10(n1)+" = "+n1+"<sub>2</sub>; ДТ =  "+dt10+" = "+n2+"<sub>2</sub>;<br><br>"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+dlme+" <div class='space'></div>ДМ<sub>дк</sub> <span class='space'></span>делимое<br>&nbsp;&nbsp;&nbsp;&nbsp;+ "+dltl+" <span class='space'></span>+ (-ДТ<sub>дк</sub>) <span class='space_3'></span>пробное вычитание<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+hg; 
+	
+	console.log("Delimoe  "+dlme);
+	console.log("Dltl  "+dltl);
+	console.log("DM+DT= "+hg);
+	
+	if(hg!=undefined) {
+		hg.split('');
+		if(hg[0]!="1") {
+			str = str + "<p style='color: red'>С<sub>0</sub>>0  При пробном вычитании возникло переполнение, деление невозможно!</p>";
+		} else {
+			str = str + " <span class='space'></span>С<sub>0</sub><0 <span class='space_2'></span>деление возможно";
+	
+			let hf = AdditionDK(hg, n2).addition();
+			str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;+ "+n2+" <span class='space'></span>+ ДТ<sub>дк</sub> <span class='space_1'></span>восстановление делимого<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+hf+" <span class='space'></span>ДМ<sub>дк</sub> <span class='space'></span>восстановленное делимое"; 
+			
+			hf = Sdvig(hf);			
+			str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+hf+" <span class='space'></span>2*(ДМ) <span class='space_5'></span>сдвиг влево (удваивание)";
+			let hd = AdditionDK(hf, dltl).addition();
+			
+			str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;+ "+dltl+" <span class='space'></span>+ (-ДТ<sub>дк</sub>) <span class='space_3'></span>вычитаем делитель";
+			
+			hd.split('');
+			let step = 6;
+			for(let i = 1; i < (step+1); i++) {
+				if (hd[0]=="0") {
+					res.push('1');
+					str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+hd+" <span class='space'></span>C<sub>"+i+"</sub>&#8805;0 <span class='space_2'></span>"+i+"-й остаток";
+					if(i<step) {
+						hd = Sdvig(hd);
+						str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+hd+" <span class='space'></span>2*C<sub>"+i+"</sub> <span class='space_2'></span>удваиваем остаток";
+						
+						hd = AdditionDK(hd, dltl).addition();
+					
+						str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;+ "+dltl+" <span class='space'></span>+ (-ДТ<sub>дк</sub>) <span class='space_3'></span>вычитаем делитель";
+					}
+				} else {
+					res.push('0');
+					str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+hd+" <span class='space'></span>C<sub>"+i+"</sub><0 <span class='space_2'></span>"+i+"-й остаток";
+					if(i<step) {
+						str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;+ "+n2+" <span class='space'></span>+ ДТ<sub>дк</sub> <span class='space_1'></span>восстановление остатка";
+					
+						hd = AdditionDK(hd, n2).addition();
+						str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+hd+" <span class='space'></span>2C<sub>"+i+"</sub> <span class='space_6'></span>восстановленный остаток";
+						hd = Sdvig(hd);
+						str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+hd+" <span class='space'></span>2*(2C<sub>"+i+"</sub>) <span class='space_5'></span>удваиваем остаток";
+						str = str + "<br>&nbsp;&nbsp;&nbsp;&nbsp;+ "+dltl+" <span class='space'></span>+ (-ДТ<sub>дк</sub>) <span class='space_3'></span>вычитаем делитель";
+						
+						hd = AdditionDK(hd, dltl).addition();
+					}
+				}
+			}
+			res = res.join('');
+			if((n1[0]=="0" && n2[0]=="0")||(n1[0]=="1" && n2[0]=="1")) {
+				res = "0."+res;
+			} else {
+				res = "1."+res; 
+			}
+			let res10 = Drob10(res);
+			hd = SdvigPr(hd, step);
+			let hd10 = Drob10(hd);
+			dt10 = Number(dt10);
+			hd10 = Number(hd10);
+			res10 = Number(res10);
+			let resultat = dt10*res10+hd10;
+			
+			str = str + "<p>ЧТ = "+res+"<sub>2</sub> = "+res10+"<sub>10</sub></p>";
+			str = str + "C<sub>"+step+"</sub> = "+ hd+" (сдвинутое вправо на "+step+" разрядов)";
+			str = str + "<br> C = C<sub><small>"+step+"</small></sub>*2<sup><small>-"+step+"</small></sup> = "+hd+" = "+hd10;
+			str = str + "<br> Проверим правильность выполненного деления:";
+			
+			str = str + "<p> ДТ*ЧТ + С = "+dt10+"*"+res10+" + "+hd10+" = "+resultat+" &#8776; ДМ</p>";
+		}
+	} else {
+		str = str + "<p style='color: red'>Переполнение, деление невозможно!</p>";
+	}
+	
+	return str;
+}
 
 var ostO = [];
 var ostO2 = [];
@@ -885,7 +1218,7 @@ Sobrat(5, n11, n12, "DK");
 Sobrat(6, n13, n14, "DK");
 Sobrat(7, n15, n16, "DK");
 
-function Count() { 
+function Count() {
 	var text = document.getElementsByTagName("input");
 	var val1 = text[0].value;
 	var val2 = text[1].value;
@@ -893,7 +1226,7 @@ function Count() {
 	arrN[0][1] = val2;
 	arrN.unshift([" ", " "]);
 
-	if(document.getElementsByClassName("items")[1].getElementsByTagName("div")[0].textContent!="ОК") {
+	if(document.getElementsByClassName("items")[1].getElementsByTagName("div")[0].textContent=="ДК") {
 		m[1].innerHTML = AdditionDK(val1, val2).result();
 
 		let div = document.createElement('div');
@@ -907,8 +1240,13 @@ function Count() {
 		It.push();
 		arr.push();
 		Sobrat(1, val1, val2, "DK");
-	} else {
+	} else if(document.getElementsByClassName("items")[1].getElementsByTagName("div")[0].textContent=="ОК") {
 		m[1].innerHTML = ProductionOK(val1, val2);
+
+		It.push();
+		arr.push();
+	} else {
+		m[1].innerHTML = DivisionPK(val1, val2);
 
 		It.push();
 		arr.push();
@@ -920,43 +1258,60 @@ function operM() {
 	if(oM.className == "operat") {
 		oM.className = oM.className + " openOperat";
 		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].className = "listOperat listShow";
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].className = "listOperat listShow listpktop";
 		oM.getElementsByTagName("i")[0].className = "up";
 		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].addEventListener('click', operL);
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].addEventListener('click', operL);
 	} else {
 		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].className = "listOperat";
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].className = "listOperat";		
 		oM.className = "operat";
 		oM.getElementsByTagName("i")[0].className = "down";
 	}
 }
 
-function operL() {
+function operL(event) {
+	
 	var oM = document.getElementsByClassName("operat")[0];
+
 	if(oM.className == "operat") {
 		oM.className = oM.className + " openOperat";
 		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].className = "listOperat listShow";
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].className = "listOperat listShow listpktop";
 		oM.getElementsByTagName("i")[0].className = " down";
 	} else {
 		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].className = "listOperat";
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].className = "listOperat";
 		oM.className = "operat";
 		oM.getElementsByTagName("i")[0].className = " up";
 	}
-	if(oM.textContent!="ОК") {
+	if(event.path[0].textContent=="ОК") {
 		oM.innerHTML = 'ОК<i class="down"></i>';
-		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].innerHTML = "ДК";
 		document.getElementsByClassName("oper")[0].innerHTML = "ОК";
 		document.getElementsByClassName("oper")[1].innerHTML = "ОК";
 		document.getElementsByName('FirstNumber')[0].placeholder='Введите число в ОК';
 		document.getElementsByName('SecondNumber')[0].placeholder='Введите число в ОК';
-	} else {		
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].innerHTML = "ДК";
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].innerHTML = "ПК";
+	} else if(event.path[0].textContent=="ДК") {		
 		oM.innerHTML = 'ДК<i class="down"></i>';
-		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].innerHTML = "ОК";
 		document.getElementsByClassName("oper")[0].innerHTML = "ДК";
 		document.getElementsByClassName("oper")[1].innerHTML = "ДК";
 		document.getElementsByName('FirstNumber')[0].placeholder='Введите число в ДК';
 		document.getElementsByName('SecondNumber')[0].placeholder='Введите число в ДК';
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].innerHTML = "ОК";
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].innerHTML = "ПК";
+	} else {
+		oM.innerHTML = 'ПК<i class="down"></i>';
+		document.getElementsByClassName("oper")[0].innerHTML = "ПК";
+		document.getElementsByClassName("oper")[1].innerHTML = "ПК";
+		document.getElementsByName('FirstNumber')[0].placeholder='Введите число в ПК';
+		document.getElementsByName('SecondNumber')[0].placeholder='Введите число в ПК';
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].innerHTML = "ДК";
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].innerHTML = "ОК";
 	}
 		document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].removeEventListener('click', operL);
-
+		document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].removeEventListener('click', operL);
 }
 
 document.body.addEventListener('click',function(e) {
@@ -964,6 +1319,7 @@ document.body.addEventListener('click',function(e) {
 		var oM = document.getElementsByClassName("operat")[0];
 		if(oM!= undefined&&oM.className != "operat") {
 			document.getElementsByClassName("items")[1].getElementsByTagName("div")[1].className = "listOperat";
+			document.getElementsByClassName("items")[1].getElementsByTagName("div")[2].className = "listOperat";
 			oM.className = "operat";
 			oM.getElementsByTagName("i")[0].className = "down";
 		}
